93f06bf3c9f6f45c4c6edf7ff96331796edcdc66
Zilonis123 <kazaks.karlis2009@gmail.com>
2023-01-12 21:11:56 +0200

    Json file and basic prototype

--- main.py
+++ main.py
@@ -1,39 +1,38 @@
 # import the dependencies
-import os
+import subprocess
 import json
 from termcolor import colored
 
-checks = [
-    {
-        "language": "python",
-        "execute": "python --version"
-    },
-    {
-        "language": "node.js",
-        "execute": "node --version"
-    },
-    {
-        "language": "c++",
-        "execute": "g++ -v"
-    }
-]
+with open("checks.json", "r", encoding="utf-8") as data_file:
+    checks = json.load(data_file).get("checks")
 
-
+found = []
+notfound = []
 for check in checks:
     # steps
     # 1. execute the command
     # 2. check if the command didn't return an error
-    # 2.1 it didn't - print that the system has the code language installed
-    # 2.2 it did - check if the user wants the not found languages to be printed if does print
+    # 2.1 it didn't - add the found language to the list
+    # 2.2 it did - add the found to the notfound
 
     # 1.
-    res = os.popen(check["execute"], "r", 0)
+    res = subprocess.run(check["execute"], shell=True)
 
+    # clear the terminal
+    subprocess.run(["cls"], shell=True)
     # 2.
-    if res.close():
+    if res.returncode == 1:
         # 2.2
+        notfound.append(check["language"])
         continue;
 
     # 2.1
-    color = colored(check["language"], "green")
-    print("[ "+ color + " ]")
+    found.append(check["language"])
+
+for language in found:
+    color = colored("+", "green")
+    print("[" + color + "] " + language + " was found.")
+
+for language in notfound:
+    color = colored("-", "red");
+    print("[" + color + "] " + language + " was NOT found.")
