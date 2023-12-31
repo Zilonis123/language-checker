3d0892cc042eb75e0a3e20d47d7497735de82c97
Zilonis123 <kazaks.karlis2009@gmail.com>
2023-12-31 15:08:58 +0200

    Shows results faster

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from termcolor import colored
 
 def check_language_installation(language_info: dict) -> bool:
     try:
-        execute_command = " ".join(language_info["execute"])
+        execute_command: str = " ".join(language_info["execute"])
         subprocess.run(execute_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         return True
     except subprocess.CalledProcessError:
@@ -14,19 +14,16 @@ def main():
     with open("checks.json", "r") as json_file:
         data: dict = json.load(json_file)
 
-    found_langs = []
 
+    plus: str = colored("+", "green")
     # Check each language
     for language_info in data["checks"]:
         found: bool = check_language_installation(language_info)
-        if found:
-            found_langs.append(language_info["language"])
-        # clearconsole()
 
-    # Display results
-    color = colored("+", "green")
-    for lang in found_langs:
-        print(f"{color} {lang.capitalize()}")
+        if found:
+            # Display results
+            lang: str = language_info["language"].capitalize()
+            print(f"{plus} {lang}")
 
 if __name__ == "__main__":
     main()
